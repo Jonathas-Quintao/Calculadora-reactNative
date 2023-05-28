2162b360fd3a74d01cdee2053cb6e01f159bca64
jonathas <jonathasquintaosilva2002@gmail.com>
2023-05-28 01:37:04 -0300

    Foi

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, Dimensions, TouchableHighlight } from 'react-native';
 
 
+
 export default props => {
     const stylesButton = [styles.button]
     if(props.double) stylesButton.push(styles.buttonDouble)
@@ -14,7 +15,6 @@ export default props => {
         </TouchableHighlight>
     )
 }
-
 
 
 
